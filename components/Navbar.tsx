7433d6e913b31b06484d9fc07134a155ff230477
godspeed7 <pawelpikus7@gmail.com>
2021-11-30 18:47:49 +0100

    home page img adjust

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -49,6 +49,7 @@ const Navbar: FunctionComponent<Props> = ({ children }) => {
             <FontAwesomeIcon icon={faYoutube} />
           </li>
         </ul>
+        {children}
       </nav>
     </header>
   );
