affce60fcdbd618f2b21d818fc0a6d5879770881
Pawel Pikus <pawelpikus7@gmail.com>
2021-12-08 09:23:51 +0100

    adding favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,10 @@ const Home: NextPage = () => {
           content="Oficjalna strona zespołu Bimber Boys... czyli retro and folk band. Naszą specjalnością są stare, niezapomniane przeboje w akustycznych aranżacjach. Inspirują nas szlagiery dwudziestolecia międzywojennego, miejski folk z różnych stron świata oraz... poetyckie ballady."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest"/>
       </Head>
       <div className={styles.container}>
         <Layout>
