1636bf48a25c30761112adb9150fdfcf511fa3d6
godspeed7 <pawelpikus7@gmail.com>
2021-12-30 14:57:13 +0100

    changing layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,25 @@
 import { Props } from "../types/props";
-import { Navbar, Footer } from "../components";
+import { Navbar, Footer, CTA } from "../components";
 import styles from "../styles/Home.module.scss";
 
 const Layout = ({ children, ...lightTheme }: Props) => {
   return (
-    <main className={styles.main}>
-      <Navbar {...lightTheme} />
-      {children}
-      <Footer {...lightTheme} />
-    </main>
+    <div className={styles.page_container}>
+      <div className={styles.header_container}>
+        <main className={styles.main}>
+          <Navbar {...lightTheme} />
+        </main>
+      </div>
+      <div className={styles.container}>
+        <main className={styles.main}>{children}</main>
+      </div>
+      <div className={styles.footer_container}>
+        <main className={styles.main}>
+          <CTA />
+          <Footer />
+        </main>
+      </div>
+    </div>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
-import { Layout, CTA } from "../components";
+import { Layout } from "../components";
 import Image from "next/image";
 import logo from "../public/bimber-logo2.png";
 
@@ -15,25 +15,37 @@ const Home: NextPage = () => {
           content="Oficjalna strona zespołu Bimber Boys... czyli retro and folk band. Naszą specjalnością są stare, niezapomniane przeboje w akustycznych aranżacjach. Inspirują nas szlagiery dwudziestolecia międzywojennego, miejski folk z różnych stron świata oraz... poetyckie ballady."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/favicons/site.webmanifest"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
       </Head>
-      <div className={styles.container}>
-        <Layout>
-          <div className={styles.img_container}>
-            <Image
-              src={logo}
-              alt="Bimber Boys logo"
-              layout="intrinsic"
-              placeholder="blur"
-              priority
-            />
-            <CTA />
-          </div>
-        </Layout>
-      </div>
+
+      <Layout>
+        <div className={styles.img_container}>
+          <Image
+            src={logo}
+            alt="Bimber Boys logo"
+            layout="intrinsic"
+            placeholder="blur"
+            priority
+          />
+        </div>
+      </Layout>
     </>
   );
 };
