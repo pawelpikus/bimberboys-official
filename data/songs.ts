07ad4cc764bf66f6c83127c3fa6e4af9e7bc08a2
Pawel Pikus <pawelpikus7@gmail.com>
2022-02-25 13:29:05 +0100

    Moving songs arr to a separate file

---
+++ data/songs.ts
@@ -0,0 +1,20 @@
+export const songs = [
+  {
+    id: 0,
+    title: "Chłopcy źli",
+    source: "/audio/chlopcy_zli.mp3",
+    duration: "04:34",
+  },
+  {
+    id: 1,
+    title: "Trzy miłości",
+    source: "/audio/trzy_milosci.mp3",
+    duration: "03:40",
+  },
+  {
+    id: 2,
+    title: "Chłopcy z naszej ulicy",
+    source: "/audio/chlopcy.mp3",
+    duration: "04:17",
+  },
+];
