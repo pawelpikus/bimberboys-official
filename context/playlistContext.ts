6be44e62e07af236cbc2b4c554bba733b502aaf4
godspeed7 <pawelpikus7@gmail.com>
2022-02-26 23:15:32 +0100

    Adding space between header and main page content.

--- context/playlistContext.ts
+++ context/playlistContext.ts
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 export const PlaylistContext = createContext({
   active: 0,
-  setActive: () => {},
+  setActive: (id: number) => {},
   handleNextTrack: () => {},
   handlePrevTrack: () => {},
 });
